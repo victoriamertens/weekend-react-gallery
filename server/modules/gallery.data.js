e8b1e7bbd3cce21f1e66ee9281a15c41a33440d3
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-24 10:35:41 -0600

    Added personal photos to gallery data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,52 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: 'images/gallery-photo-2.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: 'images/gallery-photo-3.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: 'images/gallery-photo-4.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: 'images/gallery-photo-5.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: 'images/gallery-photo-6.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: 'images/gallery-photo-1.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 7,
+    path: 'images/gallery-photo-7.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 8,
+    path: 'images/gallery-photo-8.png',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/gallery-photo-1.png" />
+    </div>
+  );
 }
 
 export default App;
