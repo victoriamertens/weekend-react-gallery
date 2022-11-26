ed82ebf24c7539184442a6bc972a949739f5a7c3
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-26 10:07:24 -0600

    GalleryItem component hooked up

---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,4 @@
+export default function GalleryItem(props) {
+  console.log('in Gallery Item');
+  return <h1>Testing</h1>;
+}
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,3 +1,22 @@
-export default function Gallery() {
-  return <h1>Testing</h1>;
+import GalleryItem from '../GalleryItem/GalleryItem.jsx';
+
+export default function GalleryList(props) {
+  console.log('in GalleryList', props.galleryItems);
+  const galleryItems = props.galleryItems;
+  const fetchGalleryItems = props.fetchGalleryItems;
+  return (
+    <>
+      <p>Testing Return</p>
+      {galleryItems.map((item) => {
+        console.log('in map');
+        return (
+          <GalleryItem
+            key={item.id}
+            fetchGalleryItems={fetchGalleryItems}
+            item={item}
+          />
+        );
+      })}
+    </>
+  );
 }
