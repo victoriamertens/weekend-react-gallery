9caca1e3218919cb7efdd21947b5b0fdd4e7f9c4
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-26 10:22:50 -0600

    Basic item render written and working

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,4 +1,28 @@
+import Axios from 'axios';
+
 export default function GalleryItem(props) {
   console.log('in Gallery Item');
-  return <h1>Testing</h1>;
+  let item = props.item;
+  console.log('item:', item);
+  let fetchGalleryItems = props.fetchGalleryItems;
+  let id = props.key;
+
+  const updateLikes = () => {
+    Axios({
+      method: 'PUT',
+      url: `/gallery/like/${id}`,
+    })
+      .then(console.log('likes updated'))
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <div>
+      <img src={item.path} />
+      <p>{item.description}</p>
+      <button>Like</button>
+      <p>{item.likes} like this!</p>
+      <p>0 people like this :(</p>
+    </div>
+  );
 }
