5a90d728f09cc083b756ba888f67172024d79da9
Victoria Mertens <victoria.mertens44@gmail.com>
2022-11-26 09:28:51 -0600

    Gallery List component successfully connected.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
 import Axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
+  const [galleryItems, setGalleryItems] = useState([]);
+
   const fetchGalleryItems = () => {
     Axios.get('/gallery')
       .then((response) => {
         console.log(response.data);
+        setGalleryItems(response.data);
+        console.log(galleryItems);
       })
       .catch((error) => {
         alert(error);
@@ -24,6 +29,10 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Gallery of My Life</h1>
       </header>
+      <GalleryList
+        galleryItems={galleryItems}
+        fetchGalleryItems={fetchGalleryItems}
+      />
       <p>Gallery goes here</p>
       <img src="images/gallery-photo-1.png" />
     </div>
